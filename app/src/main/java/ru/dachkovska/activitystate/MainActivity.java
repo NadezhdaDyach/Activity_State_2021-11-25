6541dedf165d84d886b4e9cc8ebef1bb76966f2f
Nadezhda <dachkovskanadezhda@gmail.com>
2021-11-25 16:20:05 +0900

    Initial commit

---
+++ app/src/main/java/ru/dachkovska/activitystate/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.dachkovska.activitystate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button1_onClick(View view) {
+    }
+}
